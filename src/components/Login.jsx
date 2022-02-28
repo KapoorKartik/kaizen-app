545b7beda6a4bcca2890899e3ad976082754791f
kapoorkartik <kapoorkartik110@gmail.com>
2022-02-28 11:51:54 +0530

    update the animation and enhance ux

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import {
   AlertIcon,
   Box,
   Button,
+  CircularProgress,
   FormControl,
   FormLabel,
   Input,
@@ -11,18 +12,14 @@ import Axios from "axios";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
-// import process from "dotenv/config";
-// import env from "react-dotenv";
 
 export const Login = () => {
   const [login, setLogin] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const [err, setErr] = useState(false);
 
   const navigate = useNavigate();
-
-  // const c = useContext(AuthContext);
-  // console.log("c:", c);
 
   const handleChange = ({ target: { name, value } }) => {
     setLogin({
@@ -33,37 +30,37 @@ export const Login = () => {
   };
   const { handleToken } = useContext(AuthContext);
   const handleClick = () => {
+    setIsLoading(true);
     Axios(
       `https://kapoorkartik.herokuapp.com/login/${login.username}/${login.password}`
     )
-      .then(({ data: { token }, status }) => {
+      .then(({ data: { token } }) => {
+        setIsLoading(false);
         handleToken(token);
         navigate(-1);
       })
 
       .catch((e) => {
+        setIsLoading(false);
         setErr(true);
       });
-
-    // if (login.username === "rajni" && login.password === "rajni") {
-    //   // handleToken("secret");
-    //   // navigate("/dashbord");
-    //   //handleToken("asdkjf");
-    //   handleToken("1");
-    //   navigate(-1);
-    // } else {
-    //   console.log("err");
-    // }
   };
 
   return (
     <>
-      {err == true ? (
-        <Alert status="error" position="sticky" top="65px" zIndex={2}>
+      {err === true ? (
+        <Alert
+          status="error"
+          position="sticky"
+          top="65px"
+          zIndex={2}
+          variant="solid"
+        >
           <AlertIcon />
           Wrong Username or Password
         </Alert>
       ) : null}
+
       <Box w="90%" m="auto">
         <FormControl isRequired>
           <FormLabel mt="10px">User Name</FormLabel>
@@ -86,15 +83,25 @@ export const Login = () => {
             onChange={handleChange}
           />
         </FormControl>
-        <Button
-          mt="10px"
-          w="20%"
-          color="white"
-          bg="#0077b6"
-          onClick={() => handleClick()}
-        >
-          Log In
-        </Button>
+        {isLoading === true ? (
+          <CircularProgress
+            mt="20px"
+            ml="45%"
+            isIndeterminate
+            color="green.300"
+          />
+        ) : (
+          <Button
+            mt="10px"
+            w="20%"
+            ml="45%"
+            color="white"
+            bg="#0077b6"
+            onClick={() => handleClick()}
+          >
+            Log In
+          </Button>
+        )}
       </Box>
     </>
   );
